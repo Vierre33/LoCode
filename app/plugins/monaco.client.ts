0765ffd3766668f175f246c3ada13977af0fcfa6
PY <pierre-yves.guiraudet@epitech.eu>
2026-02-15 19:26:09 +0100

    feat: remove unused code

--- app/plugins/monaco.client.ts
+++ app/plugins/monaco.client.ts
@@ -1,42 +1,5 @@
 import * as monaco from "monaco-editor";
 
-self.MonacoEnvironment = {
-    getWorker(_: any, label: string) {
-        switch (label) {
-            case "json":
-                return new Worker(
-                    new URL("monaco-editor/esm/vs/language/json/json.worker", import.meta.url),
-                    { type: "module" }
-                );
-            case "css":
-            case "scss":
-            case "less":
-                return new Worker(
-                    new URL("monaco-editor/esm/vs/language/css/css.worker", import.meta.url),
-                    { type: "module" }
-                );
-            case "html":
-            case "handlebars":
-            case "razor":
-                return new Worker(
-                    new URL("monaco-editor/esm/vs/language/html/html.worker", import.meta.url),
-                    { type: "module" }
-                );
-            case "typescript":
-            case "javascript":
-                return new Worker(
-                    new URL("monaco-editor/esm/vs/language/typescript/ts.worker", import.meta.url),
-                    { type: "module" }
-                );
-            default:
-                return new Worker(
-                    new URL("monaco-editor/esm/vs/editor/editor.worker", import.meta.url),
-                    { type: "module" }
-                );
-        }
-    },
-};
-
 export default defineNuxtPlugin(() => {
     return { provide: { monaco } };
 });
